967188d2828ddb127748b8013a116af8bd5970a3
keplib <keplib@gmail.com>
2024-09-11 17:56:31 +0200

    Initial commit, lecture 1

--- MemorizeGame/ContentView.swift
+++ MemorizeGame/ContentView.swift
@@ -9,15 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
         }
+    }
+}
+
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2))
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+           
+        }
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
